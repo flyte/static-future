ed60df90f07f263803a0a4bb0c265a7b8ef73545
Flyte <flyte@failcode.co.uk>
2015-03-29 10:34:16 +0100

    Rewrite future_imports() to use AST instead. Add tests for individual functions.

--- static_future/analysis.py
+++ static_future/analysis.py
@@ -1,54 +1,16 @@
 import __future__
-import tokenize
-
-try:
-    from StringIO import StringIO
-except ImportError:
-    from io import StringIO
+import ast
 
 
 def future_imports(code):
     """
     Return the names of all features imported from __future__.
     """
-    def get_all_imports(tokens, last_was_name=False):
-        """
-        Pop tokens until we get to a NAME type, then if it's an import
-        return it and recurse. If it's a newline or endmarker, end the
-        recursion. last_was_name makes sure that NAME tokens adjacent to
-        one another don't get returned, such as the 'as' and 'print2' tokens
-        in "print_function as print2".
-        """
-        while True:
-            t = next(tokens)
-            if t[0] == tokenize.NAME:
-                if not last_was_name:
-                    return [t[1]] + get_all_imports(tokens, True)
-            elif t[0] in (tokenize.NEWLINE, tokenize.ENDMARKER):
-                return []
-            else:
-                last_was_name = False
-
-    tokens = tokenize.generate_tokens(StringIO(code).readline)
-    try:
-        while True:
-            # from __future__ import has to be the first statement in a file
-            # https://docs.python.org/2/reference/simple_stmts.html#future
-            first_three_tokens = " ".join(
-                (next(tokens)[1], next(tokens)[1], next(tokens)[1]))
-            if first_three_tokens == "from __future__ import":
-                for imp in get_all_imports(tokens):
-                    yield imp
-            else:
-                # Keep popping tokens until the end of the _logical_ line
-                # https://docs.python.org/2/library/tokenize.html#tokenize.NL
-                while True:
-                    t = next(tokens)
-                    if t[0] in (tokenize.NEWLINE, tokenize.ENDMARKER):
-                        break
-    except StopIteration:
-        # We reached the end of the file
-        pass
+    tree = compile(code, "<string>", "exec", ast.PyCF_ONLY_AST)
+    import_froms = [x for x in tree.body if isinstance(x, ast.ImportFrom)]
+    for imp in [x for x in import_froms if x.module == "__future__"]:
+        for name in imp.names:
+            yield name.name
 
 
 def features(code):
--- tests/static_future/analysis/tests.py
+++ tests/static_future/analysis/tests.py
@@ -2,10 +2,144 @@ import __future__
 import textwrap
 import unittest
 
-from static_future.analysis import compiler_flags
+from static_future.analysis import future_imports, features, compiler_flags
 
 
-class FutureImportTest(unittest.TestCase):
+class FutureImportsTests(unittest.TestCase):
+
+    def test_extract_single_future_import(self):
+        """Extract a single import from __future__"""
+        code = textwrap.dedent("""\
+            from __future__ import print_function
+            """)
+        names = list(future_imports(code))
+        self.assertEqual(names, ["print_function"])
+
+    def test_extract_multiple_future_imports_lines(self):
+        """Extract multiple __future__ imports on separate lines"""
+        code = textwrap.dedent("""\
+            from __future__ import print_function
+            from __future__ import absolute_import
+            """)
+        names = list(future_imports(code))
+        self.assertEqual(names, ["print_function", "absolute_import"])
+
+    def test_extract_multiple_future_imports_parentheses(self):
+        """Extract multiple __future__ imports within parentheses"""
+        code = textwrap.dedent("""\
+            from __future__ import (print_function, absolute_import)
+            """)
+        names = list(future_imports(code))
+        self.assertEqual(names, ["print_function", "absolute_import"])
+
+    def test_extract_multiple_future_imports_parentheses_lines(self):
+        """Extract multiple __future__ imports within parentheses two lines"""
+        code = textwrap.dedent("""\
+            from __future__ import (print_function,
+                absolute_import)
+            """)
+        names = list(future_imports(code))
+        self.assertEqual(names, ["print_function", "absolute_import"])
+
+    def test_extract_multiple_future_imports_lines_escape(self):
+        """Extract multiple __future__ imports with newline escaped"""
+        code = textwrap.dedent("""\
+            from __future__ import print_function, \
+                absolute_import
+            """)
+        names = list(future_imports(code))
+        self.assertEqual(names, ["print_function", "absolute_import"])
+
+    def test_extract_correct_future_imports_when_imported_as_another(self):
+        """Extract only the correct imports when one is imported as another"""
+        code = textwrap.dedent("""\
+            from __future__ import print_function as absolute_import
+            """)
+        names = list(future_imports(code))
+        self.assertEqual(names, ["print_function"])
+
+    def test_extract_correct_future_imports_with_other_code(self):
+        """Extract only the correct imports when code exists in src"""
+        code = textwrap.dedent("""\
+            from __future__ import print_function
+
+            while True:
+                print("Hello, world!")
+            """)
+        names = list(future_imports(code))
+        self.assertEqual(names, ["print_function"])
+
+
+class FeaturesTests(unittest.TestCase):
+
+    def test_extract_single_future_import(self):
+        """Extract a single import from __future__"""
+        code = textwrap.dedent("""\
+            from __future__ import print_function
+            """)
+        ftures = list(features(code))
+        self.assertEqual(ftures, [__future__.print_function])
+
+    def test_extract_multiple_future_imports_lines(self):
+        """Extract multiple __future__ imports on separate lines"""
+        code = textwrap.dedent("""\
+            from __future__ import print_function
+            from __future__ import absolute_import
+            """)
+        ftures = list(features(code))
+        self.assertEqual(ftures, [
+            __future__.print_function, __future__.absolute_import])
+
+    def test_extract_multiple_future_imports_parentheses(self):
+        """Extract multiple __future__ imports within parentheses"""
+        code = textwrap.dedent("""\
+            from __future__ import (print_function, absolute_import)
+            """)
+        ftures = list(features(code))
+        self.assertEqual(ftures, [
+            __future__.print_function, __future__.absolute_import])
+
+    def test_extract_multiple_future_imports_parentheses_lines(self):
+        """Extract multiple __future__ imports within parentheses two lines"""
+        code = textwrap.dedent("""\
+            from __future__ import (print_function,
+                absolute_import)
+            """)
+        ftures = list(features(code))
+        self.assertEqual(ftures, [
+            __future__.print_function, __future__.absolute_import])
+
+    def test_extract_multiple_future_imports_lines_escape(self):
+        """Extract multiple __future__ imports with newline escaped"""
+        code = textwrap.dedent("""\
+            from __future__ import print_function, \
+                absolute_import
+            """)
+        ftures = list(features(code))
+        self.assertEqual(ftures, [
+            __future__.print_function, __future__.absolute_import])
+
+    def test_extract_correct_future_imports_when_imported_as_another(self):
+        """Extract only the correct imports when one is imported as another"""
+        code = textwrap.dedent("""\
+            from __future__ import print_function as absolute_import
+            """)
+        ftures = list(features(code))
+        self.assertEqual(ftures, [__future__.print_function])
+
+    def test_extract_correct_future_imports_with_other_code(self):
+        """Extract only the correct imports when code exists in src"""
+        code = textwrap.dedent("""\
+            from __future__ import print_function
+
+            while True:
+                print("Hello, world!")
+            """)
+        ftures = list(features(code))
+        self.assertEqual(ftures, [__future__.print_function])
+
+
+class CompilerFlagsTests(unittest.TestCase):
 
     def test_extract_single_future_import(self):
         """Extract a single import from __future__"""
